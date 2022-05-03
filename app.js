2f5d0c4a13cb7f8e3a2e73952f4d23c08a3d62df
rj-robinjoseph <robinjoseph452@gmail.com>
2022-05-03 20:22:47 +0530

    Slider Added

--- app.js
+++ app.js
@@ -4,3 +4,33 @@ var sideBar = document.querySelector(".sidebar");
 menuIcon.addEventListener("click", () => {
   sideBar.classList.toggle("small-sidebar");
 });
+
+// Slider
+
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav",
+});
+$(".slider-nav").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+  fade: true,
+  focusOnSelect: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+$("a[data-slide]").click(function (e) {
+  e.preventDefault();
+  var slideno = $(this).data("slide");
+  $(".slider-nav").slick("slickGoTo", slideno + 1);
+});
+
+$(".slider-nav").slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
