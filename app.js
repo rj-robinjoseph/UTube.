dc94e2e981fd3435bfdebea9b2d44f79b97fc915
rj-robinjoseph <robinjoseph452@gmail.com>
2022-05-04 12:51:37 +0530

    Data Fetching from Api Completed

--- app.js
+++ app.js
@@ -1,3 +1,64 @@
+const listContainer = document.querySelector(".list-container");
+
+let API_KEY = "AIzaSyCdJTAhQCzFq6VOUSOtOP6d80m8EDtGmAA";
+let VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos?";
+let CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels?";
+
+// YouTube Api
+
+fetch(
+  VIDEO_URL +
+    new URLSearchParams({
+      key: API_KEY,
+      part: "snippet",
+      chart: "mostPopular",
+      maxResults: 100,
+      regionCode: "US",
+    })
+)
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+    data.items.forEach((item) => {
+      getChannelIcon(item);
+    });
+  })
+  .catch((err) => console.log(err));
+
+const getChannelIcon = (video_data) => {
+  fetch(
+    CHANNEL_URL +
+      new URLSearchParams({
+        key: API_KEY,
+        part: "snippet",
+        id: video_data.snippet.channelId,
+      })
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      video_data.channelThumbnail =
+        data.items[0].snippet.thumbnails.default.url;
+      makeVideoCard(video_data);
+    });
+};
+
+const makeVideoCard = (data) => {
+  listContainer.innerHTML += `
+  <div class="vid-list">
+  <a href=""><img src="${data.snippet.thumbnails.high.url}" class="thumbnail" /></a>
+  <div class="flex-div">
+    <img src="${data.channelThumbnail}" />
+    <div class="vid-info">
+      <a href=""
+        >${data.snippet.title}</a
+      >
+      <p>${data.snippet.channelTitle}</p>
+    </div>
+  </div>
+</div>
+  `;
+};
+
 var menuIcon = document.querySelector(".menu-icon");
 var sideBar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
@@ -38,8 +99,3 @@ $(".slider-nav").slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
-
-// YouTube Api
-
-let API_KEY = "AIzaSyCdJTAhQCzFq6VOUSOtOP6d80m8EDtGmAA";
-let VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos";
